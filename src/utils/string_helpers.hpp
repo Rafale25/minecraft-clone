2ae9c6992ac5889a4b6aafe352a724667ffb4040
Rafale25 <rafale25.dev@gmail.com>
2024-06-19 00:03:59 +0200

    string formater helper

---
+++ src/utils/string_helpers.hpp
@@ -0,0 +1,11 @@
+#ifndef SS_HPP
+#define SS_HPP
+
+#include <sstream>
+#include <iostream>
+
+// usage:   SS("xyz" << 123 << 45.6) returning a std::string rvalue.
+#define SS(x) ( ((std::stringstream&)(std::stringstream() << x )).str())
+#define SC(x) ( ((std::stringstream&)(std::stringstream() << x )).str().c_str())
+
+#endif
