96572ac430f036e1271d27f28b73551f0291fd16
Rafale25 <rafale25.dev@gmail.com>
2024-07-07 02:09:16 +0200

    valgrind commented code

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-./build/Minecraft_Clone "127.0.0.1"
-# ./build/Minecraft_Clone "51.77.194.124"
+# ./build/Minecraft_Clone "127.0.0.1"
+./build/Minecraft_Clone "162.19.137.231"
+# valgrind --leak-check=full \
+#          --show-leak-kinds=all \
+#          --track-origins=yes \
+#          --verbose \
+#          --log-file=valgrind-out.txt \
+#          ./build/Minecraft_Clone "162.19.137.231"
