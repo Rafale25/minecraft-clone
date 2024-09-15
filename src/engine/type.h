56decba64d4ce7cc7aaab1c3ee1ef81a1288eda3
Rafale25 <rafale25.dev@gmail.com>
2024-09-15 02:26:07 +0200

    update type.h

--- src/engine/type.h
+++ src/engine/type.h
@@ -1,17 +1,20 @@
-// #pragma once
+/*
+#pragma once
 
-// #include <cstdint>
+#include <cstdint>
 
-// typedef uint64_t uint64;
-// typedef uint32_t uint32;
-// typedef uint16_t uint16;
-// typedef uint8_t uint8;
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
 
-// typedef int64_t int64;
-// typedef int32_t int32;
-// typedef int16_t int16;
-// typedef int8_t int8;
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
 
 // typedef float f32;
 // typedef double f64;
 // typedef long double f128;
+
+*/
