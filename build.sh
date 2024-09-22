21c6c971d8c84bfc8023d3007b6f7c0d76619ddd
Rafale25 <rafale25.dev@gmail.com>
2024-09-22 19:54:44 +0200

    parralel build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir build;
-cd ./build && cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release && cmake --build . && cd ..
+cd ./build && cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release && cmake --build . -j8  && cd ..
 
 # g++ src/main.cpp src/glad.c -I./include -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
