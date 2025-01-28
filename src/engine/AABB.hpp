40a579f41cb6f850a99967efc6c47cb1af967c59
Rafale25 <rafale25.dev@gmail.com>
2025-01-28 15:03:16 +0100

    Simplified code

--- src/engine/AABB.hpp
+++ src/engine/AABB.hpp
@@ -17,40 +17,50 @@ struct AABB {
         );
     }
 
-    // glm::vec3 sideDist = (sign(direction) * (mapPos - rayPos) + (sign(direction) * 0.5f) + 0.5f) * deltaDist;
-
-    // static glm::vec3 AABBtoAABBOverlapDistance(const AABB& a, const AABB& b) {
-    //     if (glm::abs(a.max.y - b.min.y) < glm::abs(a.min.y - b.max.y)) {
-    //         return (a.max.y - b.min.y);
-    //     } else {
-    //         return (a.min.y - b.max.y);
-    //     }
-    // }
-
     static glm::vec3 AABBtoAABBOverlapDistance(const AABB& a, const AABB& b) {
         glm::vec3 overlap;
 
-        // Calculate overlap distance in the x-direction
-        if (glm::abs(a.max.x - b.min.x) < glm::abs(a.min.x - b.max.x)) {
-            overlap.x = a.max.x - b.min.x;
-        } else {
-            overlap.x = a.min.x - b.max.x;
-        }
+        // Compute overlap distances for each axis
+        overlap.x = (glm::abs(a.max.x - b.min.x) < glm::abs(a.min.x - b.max.x)
+                    ? a.max.x - b.min.x
+                    : a.min.x - b.max.x);
 
-        // Calculate overlap distance in the y-direction
-        if (glm::abs(a.max.y - b.min.y) < glm::abs(a.min.y - b.max.y)) {
-            overlap.y = a.max.y - b.min.y;
-        } else {
-            overlap.y = a.min.y - b.max.y;
-        }
+        overlap.y = (glm::abs(a.max.y - b.min.y) < glm::abs(a.min.y - b.max.y)
+                    ? a.max.y - b.min.y
+                    : a.min.y - b.max.y);
 
-        // Calculate overlap distance in the z-direction
-        if (glm::abs(a.max.z - b.min.z) < glm::abs(a.min.z - b.max.z)) {
-            overlap.z = a.max.z - b.min.z;
-        } else {
-            overlap.z = a.min.z - b.max.z;
-        }
+        overlap.z = (glm::abs(a.max.z - b.min.z) < glm::abs(a.min.z - b.max.z)
+                    ? a.max.z - b.min.z
+                    : a.min.z - b.max.z);
 
         return overlap;
     }
+
+
+    // static glm::vec3 AABBtoAABBOverlapDistance(const AABB& a, const AABB& b) {
+    //     glm::vec3 overlap;
+
+    //     // Calculate overlap distance in the x-direction
+    //     if (glm::abs(a.max.x - b.min.x) < glm::abs(a.min.x - b.max.x)) {
+    //         overlap.x = a.max.x - b.min.x;
+    //     } else {
+    //         overlap.x = a.min.x - b.max.x;
+    //     }
+
+    //     // Calculate overlap distance in the y-direction
+    //     if (glm::abs(a.max.y - b.min.y) < glm::abs(a.min.y - b.max.y)) {
+    //         overlap.y = a.max.y - b.min.y;
+    //     } else {
+    //         overlap.y = a.min.y - b.max.y;
+    //     }
+
+    //     // Calculate overlap distance in the z-direction
+    //     if (glm::abs(a.max.z - b.min.z) < glm::abs(a.min.z - b.max.z)) {
+    //         overlap.z = a.max.z - b.min.z;
+    //     } else {
+    //         overlap.z = a.min.z - b.max.z;
+    //     }
+
+    //     return overlap;
+    // }
 };
