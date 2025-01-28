828dc02cccc29e6c3c9398722bc6fa4526de8caf
Rafale25 <rafale25.dev@gmail.com>
2025-01-28 16:56:35 +0100

    comment line

--- src/game/scenes/GameView.cpp
+++ src/game/scenes/GameView.cpp
@@ -129,8 +129,7 @@ void GameView::playerMovements(float dt)
 
     glm::vec3 player_feet_position = camera.getPosition() - glm::vec3(0.0f, player_height, 0.0f);
 
-    printf("Y: %.6f %.6f %.6f\n", glm::mod(player_feet_position.x, 1.0f), glm::mod(player_feet_position.y, 1.0f), glm::mod(player_feet_position.z, 1.0f));
-    // printf("Y: %.5f\n", glm::mod(player_feet_position.y, 1.0f));
+    // printf("Y: %.6f %.6f %.6f\n", glm::mod(player_feet_position.x, 1.0f), glm::mod(player_feet_position.y, 1.0f), glm::mod(player_feet_position.z, 1.0f));
 
     AABB player_aabb_under_feet = {
         player_feet_position + glm::vec3(-player_radius+0.01f, -0.01f, -player_radius+0.01f),
