098c81075d2f534c474eadda6cd66d35a7113cf9
Rafale25 <rafale25.dev@gmail.com>
2025-02-26 22:12:18 +0100

    added void arg to fix warnings

--- src/utils/mem_info.h
+++ src/utils/mem_info.h
@@ -19,13 +19,13 @@ extern "C" {
  * memory use) measured in bytes, or zero if the value cannot be
  * determined on this OS.
  */
-size_t getPeakRSS();
+size_t getPeakRSS(void);
 
 /**
  * Returns the current resident set size (physical memory use) measured
  * in bytes, or zero if the value cannot be determined on this OS.
  */
-size_t getCurrentRSS();
+size_t getCurrentRSS(void);
 
 
 #ifdef __cplusplus
