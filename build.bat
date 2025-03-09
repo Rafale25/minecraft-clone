18a80f372daeb818762245df43bcbe4d0bec66cf
Rafale25 <rafale25.dev@gmail.com>
2025-03-09 01:05:03 +0100

    update

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
+@REM mkdir build;
+@REM cd ./build && cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release && cmake --build . -j  && cd ..
+
 mkdir build;
-cd ./build && cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release && cmake --build . -j  && cd ..
+cmake --build ./build -j
+
+@REM --config Release
+@REM cmake --build build -j --preset release
