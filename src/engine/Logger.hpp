f7526fb9e65dda99b182193af275fecf8c846cf1
Rafale25 <rafale25.dev@gmail.com>
2025-04-06 10:26:57 +0200

    Logger

---
+++ src/engine/Logger.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdarg>
+#include <cstdio>
+#include <print>
+#include <chrono>
+
+#define LOG_TIME_FORMAT "{:%H:%M:%S}"
+#define _CHRONO std::chrono::floor<std::chrono::milliseconds>(std::chrono::current_zone()->to_local(std::chrono::system_clock::now())) // TODO: current_zone()->to_local doesn't seem to work
+
+#define _log(x, color, level, ...)  (std::print("\033[95m[" LOG_TIME_FORMAT "]\033[0m " "\033[" color "m[" level "] " x "\033[0m\n", _CHRONO, ##__VA_ARGS__))
+
+#define logT(x, ...) _log(x, "96", "Trace", ##__VA_ARGS__)
+#define logD(x, ...) _log(x, "94", "Debug", ##__VA_ARGS__)
+#define logI(x, ...) _log(x, "92", "Info", ##__VA_ARGS__)
+#define logW(x, ...) _log(x, "93", "Warn", ##__VA_ARGS__)
+#define logE(x, ...) _log(x, "91", "Error", ##__VA_ARGS__)
+#define logF(x, ...) _log(x, "101", "Fatal", ##__VA_ARGS__)
+
+// template <typename... Args>
+// inline void my_print(std::format_string<Args...> fmt, Args&&... args) {
+//     std::print(fmt, std::forward<Args>(args)...);
+// }
