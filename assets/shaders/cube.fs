671efb7df5da42fab10954212090d39e2220e56e
Rafale25 <rafale25.dev@gmail.com>
2025-09-09 20:38:35 +0200

    simplified shaders using includes

--- assets/shaders/cube.fs
+++ assets/shaders/cube.fs
@@ -83,78 +83,7 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal)
     return shadow;
 }
 
-vec3 Uncharted2Tonemap(vec3 x) {
-	float Brightness = 0.28;
-	x*= Brightness;
-	float A = 0.28;
-	float B = 0.29;
-	float C = 0.10;
-	float D = 0.2;
-	float E = 0.025;
-	float F = 0.35;
-	return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
-}
-
-vec3 unchartedTonemapping(vec3 color)
-{
-	vec3 curr = Uncharted2Tonemap(color*4.7);
-	color = curr/Uncharted2Tonemap(vec3(15.2));
-	return color;
-}
-
-vec3 PBRNeutralToneMapping( vec3 color ) {
-  const float startCompression = 0.8 - 0.04;
-  const float desaturation = 0.15;
-
-  float x = min(color.r, min(color.g, color.b));
-  float offset = x < 0.08 ? x - 6.25 * x * x : 0.04;
-  color -= offset;
-
-  float peak = max(color.r, max(color.g, color.b));
-  if (peak < startCompression) return color;
-
-  const float d = 1. - startCompression;
-  float newPeak = 1. - d * d / (peak + d - startCompression);
-  color *= newPeak / peak;
-
-  float g = 1. - 1. / (desaturation * (peak - newPeak) + 1.);
-  return mix(color, newPeak * vec3(1, 1, 1), g);
-}
-
-vec3 lottes(vec3 x) {
-  x *= vec3(0.9); //I reduced the light a little
-  const vec3 a = vec3(1.6);
-  const vec3 d = vec3(0.977);
-  const vec3 hdrMax = vec3(8.0);
-  const vec3 midIn = vec3(0.18);
-  const vec3 midOut = vec3(0.267);
-
-  const vec3 b =
-	  (-pow(midIn, a) + pow(hdrMax, a) * midOut) /
-	  ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
-  const vec3 c =
-	  (pow(hdrMax, a * d) * pow(midIn, a) - pow(hdrMax, a) * pow(midIn, a * d) * midOut) /
-	  ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
-
-  return pow(x, a) / (pow(x, a * d) * b + c);
-}
-
-vec3 toLinearSRGB(vec3 sRGB)
-{
-	bvec3 cutoff = lessThan(sRGB, vec3(0.04045));
-	vec3 higher = pow((sRGB + vec3(0.055))/vec3(1.055), vec3(2.4));
-	vec3 lower = sRGB/vec3(12.92);
-	return mix(higher, lower, cutoff);
-}
-
-vec3 fromLinearToSRGB(vec3 linearRGB)
-{
-	bvec3 cutoff = lessThan(linearRGB, vec3(0.0031308));
-	vec3 higher = vec3(1.055)*pow(linearRGB, vec3(1.0/2.4)) - vec3(0.055);
-	vec3 lower = linearRGB * vec3(12.92);
-
-	return mix(higher, lower, cutoff);
-}
+#include "tonemapping.glsl"
 
 void main()
 {
