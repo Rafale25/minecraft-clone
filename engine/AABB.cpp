e1d7f7edff3f4f7c9f3f3aa1b026de10ef53d986
Rafale25 <rafale25.dev@gmail.com>
2025-09-10 00:27:49 +0200

    AABB.hpp -> cpp

---
+++ engine/AABB.cpp
@@ -0,0 +1,23 @@
+#include "AABB.hpp"
+
+#include <glm/ext/vector_bool3.hpp>
+#include <glm/common.hpp>
+
+bool AABB::AABBtoAABB(const AABB& a, const AABB& b) {
+    return (
+        a.max.x > b.min.x &&
+        a.min.x < b.max.x &&
+        a.max.y > b.min.y &&
+        a.min.y < b.max.y &&
+        a.max.z > b.min.z &&
+        a.min.z < b.max.z
+    );
+}
+
+glm::vec3 AABB::AABBtoAABBOverlapDistance(const AABB& a, const AABB& b) {
+    const glm::vec3 min_minus_max = a.max - b.min;
+    const glm::vec3 max_minus_min = a.min - b.max;
+    const glm::bvec3 r = glm::lessThan(glm::abs(min_minus_max), glm::abs(max_minus_min));
+
+    return glm::mix(max_minus_min, min_minus_max, r);
+}
--- engine/AABB.hpp
+++ engine/AABB.hpp
@@ -1,28 +1,10 @@
 #pragma once
 
 #include <glm/ext/vector_float3.hpp>
-#include <glm/ext/vector_bool3.hpp>
-#include <glm/common.hpp>
 
 struct AABB {
 	glm::vec3 min, max;
 
-    static bool AABBtoAABB(const AABB& a, const AABB& b) {
-        return (
-            a.max.x > b.min.x &&
-            a.min.x < b.max.x &&
-            a.max.y > b.min.y &&
-            a.min.y < b.max.y &&
-            a.max.z > b.min.z &&
-            a.min.z < b.max.z
-        );
-    }
-
-    static glm::vec3 AABBtoAABBOverlapDistance(const AABB& a, const AABB& b) {
-        const glm::vec3 min_minus_max = a.max - b.min;
-        const glm::vec3 max_minus_min = a.min - b.max;
-        const glm::bvec3 r = glm::lessThan(glm::abs(min_minus_max), glm::abs(max_minus_min));
-
-        return glm::mix(max_minus_min, min_minus_max, r);
-    }
+    static bool AABBtoAABB(const AABB& a, const AABB& b);
+    static glm::vec3 AABBtoAABBOverlapDistance(const AABB& a, const AABB& b);
 };
