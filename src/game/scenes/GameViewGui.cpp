ef1019ae1c992b816b037060e2bb4ec704a789b6
Rafale25 <rafale25.dev@gmail.com>
2025-09-30 00:44:53 +0200

    shadowmap checkbox

--- src/game/scenes/GameViewGui.cpp
+++ src/game/scenes/GameViewGui.cpp
@@ -47,10 +47,6 @@ void GameView::gui(float dt)
 {
     // ImGui::ShowDemoWindow();
 
-    // ImGui::Begin("Shadow map");
-    // ImGui::Image((ImTextureID)(intptr_t) world_renderer.shadowmap._depthTexture._texture, ImVec2(ctx.width/3, ctx.height/3), ImVec2(0, 1), ImVec2(1, 0));
-    // ImGui::End();
-
     if (block_selection_mode) {
         guiWorldEdit();
     }
@@ -58,6 +54,15 @@ void GameView::gui(float dt)
     ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f), ImGuiCond_Always, ImVec2(0.0f, 0.0f));
 
     ImGui::Begin("Debug", nullptr, !_cursor_enabled ? ImGuiWindowFlags_NoInputs : 0);
+
+    static bool _shadowmap_texture = false;
+    ImGui::Checkbox("ShadowMap texture", &_shadowmap_texture);
+    if (_shadowmap_texture) {
+        ImGui::Begin("Shadow map");
+        // ImGui::Image((ImTextureID)(intptr_t) world_renderer.shadowmap._depthTexture._texture, ImVec2(ctx.width/3, ctx.height/3), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::Image((ImTextureID)(intptr_t) world_renderer.shadowmap._depthTexture._texture, ImVec2(world_renderer.shadowmap._shadowmap_size/4, world_renderer.shadowmap._shadowmap_size/4), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+    }
 
     ImGui::Checkbox("Profiler", &_show_profiler_gui);
 
