7202050a00963af6c3b3329db6b4b59b86d99719
Rafale25 <rafale25.dev@gmail.com>
2026-01-03 13:11:31 +0100

    uniforms includes

--- assets/shaders/uniforms.glsl
+++ assets/shaders/uniforms.glsl
@@ -1,28 +1,8 @@
+#define MAT4 mat4
+#define VEC4 vec4
+#define VEC2 vec2
+#define INIT(x)
+
 layout(std430, binding=3) readonly buffer uniformsBuffer {
-    mat4 projection;
-    mat4 view;
-    mat4 projection_view;
-    mat4 lightSpaceMatrix;
-    mat4 lightSpaceMatrices[4]; // cascaded shadowmapping
-    vec4 sunDirection;
-    vec4 viewPosition;
-    float cascadePlaneDistances[4];
-    vec2 resolution;
-    float sunDotAngle;
-    float FOV;
-    float fogDensity;
-    float shadow_bias;
-    float ambient_occlusion_strength;
-    float time;
-    float exposure;
-    float volumetricDensity;
-    float volumetricHGphaseFront;
-    float volumetricHGphaseBack;
-    float volumetricAmbiantLight;
-    int ambient_occlusion_enabled;
-    int tonemapping_enabled;
-    int cascadeCount;
-    int shadows_enabled;
-    float TEST_SLIDER_0;
-    float TEST_SLIDER_1;
+#include "../../src/game/renderer/uniforms_raw"
 } uniforms;
--- src/game/renderer/uniforms_struct.hpp
+++ src/game/renderer/uniforms_struct.hpp
@@ -3,7 +3,16 @@
 #include <glm/detail/type_mat4x4.hpp>
 #include <glm/detail/type_vec2.hpp>
 
+#define MAT4 glm::mat4
+#define VEC4 glm::vec4
+#define VEC2 glm::vec2
+#define INIT(x) {x}
+
 struct uniformsParameters {
+    #include "uniforms_raw"
+};
+
+/*
     glm::mat4 projection = glm::mat4(1.0f);
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection_view = glm::mat4(1.0f);
@@ -31,4 +40,4 @@ struct uniformsParameters {
     int shadows_enabled = 0;
     float TEST_SLIDER_0 = 0.0f;
     float TEST_SLIDER_1 = 0.0f;
-};
+*/
